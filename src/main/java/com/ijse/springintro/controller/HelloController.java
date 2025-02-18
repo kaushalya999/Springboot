44d6c1265b2cde75838246162f54d02e86ea55ec
kaushalya999 <kaushalyaperera127@gmail.com>
2025-02-17 20:15:47 -0800

    create post mapping api for same hello endpoint

--- src/main/java/com/ijse/springintro/controller/HelloController.java
+++ src/main/java/com/ijse/springintro/controller/HelloController.java
@@ -2,14 +2,21 @@ package com.ijse.springintro.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String sayHello(){
-        return "hello Sring Boot";
+        return "hello Spring Boot";
     }
 
+    @PostMapping("/hello")
+    public String postRequest() {
+        return "This is a POST request to hello endpoint";
+    }
     
 }
