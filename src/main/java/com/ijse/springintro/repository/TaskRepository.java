e38b70e4559d9cb7eaba63e107388cca0ce600cc
kaushalya999 <kaushalyaperera127@gmail.com>
2025-02-23 06:00:42 +0530

    TaskRepository extends JpaRepository

--- src/main/java/com/ijse/springintro/repository/TaskRepository.java
+++ src/main/java/com/ijse/springintro/repository/TaskRepository.java
@@ -1,8 +1,11 @@
 package com.ijse.springintro.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.ijse.springintro.entity.Task;
+
 @Repository
-public interface TaskRepository {
-    
+public interface TaskRepository extends JpaRepository<Task,  Long>{
+    //custome queries can be defined here
 }
