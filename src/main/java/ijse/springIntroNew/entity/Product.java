6d90a27ad221d4d3e08626aabe631dfceb0671f0
Kaushalya Perera <kaushalyaperera127@gmail.com>
2026-01-25 16:20:20 +0530

    add category and product entity and map them one to many

---
+++ src/main/java/ijse/springIntroNew/entity/Product.java
@@ -0,0 +1,33 @@
+package ijse.springIntroNew.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
